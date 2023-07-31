a9cbe94292f1c8ed8487fc8d06f8c953f45bccb1
Nahuel <nanualejandro@gmail.com>
2023-07-31 19:22:25 -0300

    Trabajo terminado

--- PreEntrega3/index.js
+++ PreEntrega3/index.js
@@ -3,9 +3,33 @@ const app = express()
 const PORT = 8080
 const Contenedor = require("../PreEntrega2/Contenedor")
 
-const testContendor = async () => {
-    const contenedor = new Contenedor("productos.txt")
+const contenedor = new Contenedor("productos.txt")
 
+app.get("/productos", async (req, res) => {
+    try {
+        const productos = await contenedor.getAll()
+        res.json(productos)
+    } catch (error) {
+        res.status(500).json({ error: "Error al obtener los productos" })
+    }
+})
+
+app.get("/productoRandom", async (req, res) => {
+    try {
+        const productos = await contenedor.getAll()
+        if (productos.length === 0) {
+            res.status(404).json({ error: "No hay productos disponibles" })
+        } else {
+            const randomIndex = Math.floor(Math.random() * productos.length)
+            const randomProduct = productos[randomIndex]
+            res.json(randomProduct)
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Error al obtener un producto aleatorio" })
+    }
+})
+
+const initializeData = async () => {
     const producto1 = {
         title: 'Producto 1',
         price: 100,
@@ -26,18 +50,13 @@ const testContendor = async () => {
         thumbnail: 'url3',
     }
     await contenedor.save(producto3)
-
-    const productos = await contenedor.getAll()
-
-    app.get("/productos", (req, res) => {
-        res.send("Los productos son: ", productos)
-    })
 }
 
-testContendor()
+initializeData().then(() => {
+    const server = app.listen(PORT, () => {
+        console.log(`Escuchando en el puerto ${PORT}`)
+    })
 
-const server = app.listen(PORT, () => {
-    console.log(`Escuchando en el puerto ${PORT}`)
+    server.on("error", error => console.log(`Error en servidor ${error}`))
 })
 
-server.on("error", error => console.log(`Error en servidor ${error}`))

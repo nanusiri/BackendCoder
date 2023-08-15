717e31226cdd0556ba8a80deb096baad43d4e9c2
Nahuel <nanualejandro@gmail.com>
2023-08-15 19:55:00 -0300

    Ya resolvi que se vea, pero no me esta tomando lo que escribo en el input, la funcion que esta en onclick no se me pinta de amarilo

--- ProyectoFinal2/src/app.js
+++ ProyectoFinal2/src/app.js
@@ -1,13 +1,24 @@
 const express = require('express')
-const app = express()
-const http = require('http').createServer(app)
-const io = require('socket.io')(http)
-const path = require("path")
-const PORT = 8080
+const http = require('http')
+const socketIo = require('socket.io')
+const path = require('path')
+const { Server } = require('socket.io')
+const handlebars = require('express-handlebars')
 
-app.use(express.static(path.join(__dirname, "/public/index.hbs")))
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+
+app.engine("handlebars", handlebars.engine())
+app.set("views", __dirname + "/views")
+app.set("view engine", "handlebars")
+app.use(express.static(path.join(__dirname, 'public')))
 
 let products = []
+
+app.get("/", (req, res) => {
+    res.render("index.hbs")
+})
 
 io.on("connection", (socket) => {
     socket.emit("productList", products)
@@ -19,6 +30,7 @@ io.on("connection", (socket) => {
     })
 })
 
-http.listen(PORT, () => {
+const PORT = process.env.PORT || 8080
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })

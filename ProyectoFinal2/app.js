b8e8cf039c3492116130039193ac8b529d4b69fb
Nahuel <nanualejandro@gmail.com>
2023-08-15 00:24:38 -0300

    me trabe y no me corre la app en el localhost:8080

--- ProyectoFinal2/app.js
+++ ProyectoFinal2/app.js
@@ -5,7 +5,7 @@ const io = require('socket.io')(http)
 const path = require("path")
 const PORT = 8080
 
-app.use(express.static(path.join(__dirname, "/public")))
+app.use(express.static(path.join(__dirname, "/public/index.hbs")))
 
 let products = []
 
@@ -17,4 +17,8 @@ io.on("connection", (socket) => {
         products.push(product)
         io.emit("newProduct", { producto: product })
     })
+})
+
+http.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
--- ProyectoFinal2/src/public/client.js
+++ ProyectoFinal2/src/public/client.js
@@ -2,3 +2,23 @@ const socket = io()
 
 const boton = document.getElementById("boton")
 boton.addEventListener("click", sendProduct())
+
+function sendProduct() {
+    const product = document.getElementById("productInput").value
+    socket.emit("newProduct", product)
+}
+
+function appendProduct(product) {
+    const productList = document.getElementById("productList")
+    const newProduct = document.createElement("p")
+    newProduct.textContent = `Producto: ${product}`
+    productList.appendChild(newProduct)
+}
+
+socket.on("productList", (products) => {
+    const productList = document.getElementById("productList")
+    productList.innerHTML = ""
+    products.forEach((prod) => {
+        appendProduct(prod.producto)
+    });
+})

ac7ac664f74222941168c9259b86ac12f86f84b6
Nahuel <nanualejandro@gmail.com>
2023-12-09 17:37:47 -0300

    Clase swagger

---
+++ RecursosBackend-Adoptme-main/src/app.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import swaggerJsdoc from 'swagger-jsdoc';
+import SwaggerUiExpress from 'swagger-ui-express';
+import __dirname from './utils/index.js';
+
+import usersRouter from './routes/users.router.js';
+import petsRouter from './routes/pets.router.js';
+import adoptionsRouter from './routes/adoption.router.js';
+import sessionsRouter from './routes/sessions.router.js';
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+const connection = mongoose.connect(`mongodb+srv://nanualejandro:UaQAnwVjBAMsE6PN@coderhouse.brwecw3.mongodb.net/?retryWrites=true&w=majority`)
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.1',
+        info: {
+            title: "Documentacion del poder y del saber",
+            description: "API pensada para la clase de Swagger"
+        }
+    },
+    apis: [`src/docs/Users.yaml`]
+}
+
+const specs = swaggerJsdoc(swaggerOptions)
+app.use('/apidocs', SwaggerUiExpress.serve, SwaggerUiExpress.setup(specs))
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/users', usersRouter);
+app.use('/api/pets', petsRouter);
+app.use('/api/adoptions', adoptionsRouter);
+app.use('/api/sessions', sessionsRouter);
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
